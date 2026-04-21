ae5944656f87b88192bebfa0d8645c405fc3e6d9
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2026-04-21 19:20:01 -0400

    [add] - routage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
 export function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={
+          <>
+            <h1>Home</h1>
+            <Link to={"/about"}></Link>
+          </>
+          } />
         <Route path="/about" element={<h1>About</h1>} />
       </Routes>
     </>
