f6271a9dd30e0458576f7e3debe7d045d5495cd6
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2026-04-21 19:10:11 -0400

    [add] - routage

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export function App() {
     <>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
       </Routes>
     </>
   )
