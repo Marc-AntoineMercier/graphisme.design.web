ac2bd8aea8f004203f415bf8d14b51fb404a7eee
Marc-Antoine Mercier <marc.mercier.mam@gmail.com>
2026-04-21 11:32:36 -0400

    [update] - text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ export function App() {
 
   return (
     <>
-      
+      <h1>Welcome to React with TypeScript!</h1>
+      <p>This is a simple React application using TypeScript.</p>
     </>
   )
 }
