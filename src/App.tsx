8684f7dc25fbae3f5f7d2cc628ceec0563357f01
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2026-04-22 09:06:10 -0400

    [update] - footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { HomePage, AboutPage, GraveyardPage, DetailGraveyardPage } from '@/pages';
+import { Navbar, Footer } from '@/components';
 
 export function App() {
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/graveyard" element={<GraveyardPage />} />
         <Route path="/graveyard/:id" element={<DetailGraveyardPage />}/>
       </Routes>
+      <Footer />
     </>
   )
 }
