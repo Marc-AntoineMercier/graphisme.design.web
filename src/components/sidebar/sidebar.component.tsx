d038ee3fa734d74279a76a950017d0f7d980113c
Marc-AntoineMercier <marc.mercier.mam@gmail.com>
2026-04-22 20:25:59 -0400

    [add] - mui for the drawer component

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -4,27 +4,19 @@ import "./sidebar.css"
 export type SidebarPosition = "left" | "right"
 
 export type SidebarProps = {
-    children: ReactNode;
-    isOpen: boolean;
-    onClose: () => void;
-    position?: SidebarPosition;
-    width?: string
+  children: ReactNode;
 }
 
-export function Sidebar({ children, isOpen, onClose, position = "right", width = "300px" }: SidebarProps) {
-    return (
-        <div 
-            className={`sidebar ${isOpen ? "open" : ""}`} 
-            style={{
-                [position]: isOpen ? "0" : `-${width}`,
-                width: width,
-            }}
-        >
-            <button className="sidebar-close" onClick={onClose}>
-                &times;
-            </button>
-            <div className="sidebar-content">{children}</div>
-        </div>
-    )
+export function Sidebar({ children }: SidebarProps) {
+
+  return (
+    <div>
+      <div>
+      </div>
+      <div className="sidebar">
+        {children}
+      </div>
+    </div>
+  )
 
 }
