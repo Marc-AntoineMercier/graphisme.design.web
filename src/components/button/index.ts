31e5aaac442afc9b507e8b05007bdb62a19e2cdc
Marc-Antoine Mercier <marc.mercier.mam@gmail.com>
2026-04-29 21:31:57 -0400

    [update] - struct

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,3 +1,3 @@
-export * from "./close.component";
-export * from "./search.component";
-export * from "./navigation.component";
+export * from "./close.button.component";
+export * from "./search.button.component";
+export * from "./navigation.button.component";
