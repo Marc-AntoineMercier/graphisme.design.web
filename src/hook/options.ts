7065d978562a60c46f23974eb937e0b6f0f00bed
Marc-Antoine Mercier <marc.mercier.mam@gmail.com>
2026-05-02 13:11:26 -0400

    [fix] - auto closing when changing page

--- src/hook/options.ts
+++ src/hook/options.ts
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export const useOptions = () => {
     const [open, setOpen] = useState<boolean>(false);
+    const location = useLocation();
 
     const toggleDrawer = (state: boolean) => () => {
         setOpen(state);
     }
+
+    useEffect(() => {
+        setOpen(false);
+    }, [location.pathname]);
 
     return {
         state: open,
