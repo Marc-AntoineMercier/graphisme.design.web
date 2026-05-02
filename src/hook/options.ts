6fae34643117e084a338ab46f52b0d463780d95b
Marc-Antoine Mercier <marc.mercier.mam@gmail.com>
2026-05-02 13:14:59 -0400

    [update] - formattage

--- src/hook/options.ts
+++ src/hook/options.ts
@@ -1,21 +1,27 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export const useOptions = () => {
-    const [open, setOpen] = useState<boolean>(false);
-    const location = useLocation();
+export type UseOptionsContext = {
+  autoClose?: boolean;
+}
 
-    const toggleDrawer = (state: boolean) => () => {
-        setOpen(state);
-    }
+export const useOptions = ({ autoClose = true }: UseOptionsContext?) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const location = useLocation();
 
-    useEffect(() => {
-        setOpen(false);
-    }, [location.pathname]);
+  const toggleDrawer = (state: boolean) => () => {
+    setOpen(state);
+  }
 
-    return {
-        state: open,
-        close: toggleDrawer(false),
-        open : toggleDrawer(true)
-    }
+  useEffect(() => {
+    if (autoClose) {
+      setOpen(false);
+    }  
+  }, [location.pathname]);
+
+  return {
+    state: open,
+    close: toggleDrawer(false),
+    open : toggleDrawer(true)
+  }
 }
